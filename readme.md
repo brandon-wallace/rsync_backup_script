b36819ce5708d9b4218522dd941a53b23d2f2575
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-09-17 17:50:27 -0400

    First commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# rsync_backup_script
+
+Use Rsync to backup your files with logging to a CSV file.
---
+++ rsync_backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -
+
+shopt -s lastpipe
+
+PORT=22
+
+SSH_CMD="ssh -v -p $PORT -i /home/$USER/.ssh/id_rsa"
+
+SOURCE=$HOME
+
+DESTINATION='/mnt/backup/'
+
+TODAY="$(date +%Y-%m-%d-%H%M%S)"
+
+
+if [ -d $DESTINATION ]; then
+    printf "%s\n" "BACKUP DRIVE MOUNTED."
+else
+    printf "%s\n" "MOUNTING BACKUP DRIVE..."
+    mount -v $DESTINATION
+    if [ "$(echo $?)" -eq 0 ]; then
+        printf "%s\n" "${TODAY},'ERROR! BACKUP DRIVE NOT ACCESSIBLE.'" >> backup_log.csv
+        exit 1
+    fi
+fi
+
+
+printf "%s\n" "PERFORMING BACKUP. PLEASE WAIT..."
+
+RSYNC_COPY="-a --stats --human-readable --progress --checksum --exclude={'__pycache__/','.pytest_cache/','.venv/','node_modules/','*.pyc'} --log-file=backup_$(date +%Y-%m-%d-%H%M%S).log"
+
+START="$(date +%Hh:%Mm:%Ss)"
+
+printf "%s" "$TODAY,$START" >> backup_log.csv
+
+rsync $SSH_CMD $RSYNC_COPY $SOURCE $DESTINATION | { awk '/total size is/{print $4}' | read TOTAL_SIZE; }
+
+FINISH="$(date +%Hh:%Mm:%Ss)"
+
+printf "%s\n" ",$FINISH,$TOTAL_SIZE,$SOURCE,$DESTINATION" >> backup_log.csv
+
+printf "%s\n" "BACKUP COMPLETE."
+

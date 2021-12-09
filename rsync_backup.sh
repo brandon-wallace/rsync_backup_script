b4fd2a553bfe379a17b14741c2c44a94e7644215
Brandon Wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-12-09 17:35:32 -0500

    Add logging to /var/log/messages
    Add logging to /var/log/messages

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -25,27 +25,32 @@ else
 fi
 
 
-printf "%s\n" "PERFORMING BACKUP. PLEASE WAIT..."
+function run_backup() {
+    printf "%s\n" "PERFORMING BACKUP. PLEASE WAIT..."
 
-RSYNC_COPY="-a --stats --human-readable --progress --checksum --exclude={'__pycache__/','.pytest_cache/','.venv/','node_modules/','*.pyc'} --log-file=backup_$(date +%Y-%m-%d_%H%M%S).log"
+    RSYNC_COPY="-a --stats --human-readable --progress --checksum --exclude={'__pycache__/','.pytest_cache/','.venv/','node_modules/','*.pyc'} --log-file=backup_$(date +%Y-%m-%d_%H%M%S).log"
 
-START="$(date +%Hh:%Mm:%Ss)"
+    START="$(date +%Hh:%Mm:%Ss)"
+    
+    logger "STARTING BACKUP: $START"
 
-BACKUP_START=$SECONDS
+    BACKUP_START=$SECONDS
 
-printf "%s" "$TODAY,$START" >> $HOME/backup_log.csv
+    printf "%s" "$TODAY,$START" >> $HOME/backup_log.csv
 
-rsync -e $SSH_CMD $RSYNC_COPY $SOURCE $DESTINATION | { awk '/total size is/{print $4}' | read TOTAL_SIZE; }
+    rsync -e $SSH_CMD $RSYNC_COPY $SOURCE $DESTINATION | { awk '/total size is/{print $4}' | read TOTAL_SIZE; }
 
-BACKUP_FINISH=$SECONDS
+    BACKUP_FINISH=$SECONDS
 
-TOTAL_SECONDS="$((BACKUP_FINISH - BACKUP_START))"
+    TOTAL_SECONDS="$((BACKUP_FINISH - BACKUP_START))"
 
-ELAPSED_TIME="$(($TOTAL_SECONDS / 3600))h:$(($TOTAL_SECONDS / 60))m:$(($TOTAL_SECONDS % 60))s"
+    ELAPSED_TIME="$(($TOTAL_SECONDS / 3600))h:$(($TOTAL_SECONDS / 60))m:$(($TOTAL_SECONDS % 60))s"
 
-FINISH="$(date +%Hh:%Mm:%Ss)"
+    FINISH="$(date +%Hh:%Mm:%Ss)"
+    
+    logger "FINISHING BACKUP: $FINISH"
 
-printf "%s\n" ",$FINISH,$ELAPSED_TIME,$TOTAL_SIZE,$SOURCE,$DESTINATION" >> $HOME/backup_log.csv
+    printf "%s\n" ",$FINISH,$ELAPSED_TIME,$TOTAL_SIZE,$SOURCE,$DESTINATION" >> $HOME/backup_log.csv
 
-printf "%s\n" "BACKUP COMPLETE."
-
+    printf "%s\n" "BACKUP COMPLETE."
+}
